2d332ee6a1bdd500332a7a1ed8dcb15e61993e79
lcsaria <lmarlsaria21@gmail.com>
2021-10-18 11:47:36 +0800

    grades

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://d934-112-198-179-122.ngrok.io/api/v1'
+    baseURL: 'http://4123-2001-fd8-b2d0-9c58-f902-36ee-4696-c8f1.ngrok.io/api/v1'
 });
--- src/components/grades/table/MyGrades.js
+++ src/components/grades/table/MyGrades.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import { Spinner } from 'react-bootstrap'
 import api from '../../../api/axios'
 
 function EnrolledSubjects() {
@@ -6,40 +7,47 @@ function EnrolledSubjects() {
   const [data, setData] = useState([])
   const [grades, setGrades] = useState([])
   const [isGrade, setIsGrade] = useState(false)
+  const [isSelected, setSelected] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+
   useEffect(() => {
     console.log(grades)
     const tae = async () => {
       await api.get(`semforgrades/${id}`)
       .then(result => {
-        setData(result.data);
+        setData(result.data)
         setIsGrade(true)
-        console.log(result.data);
+        console.log(result.data)
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
         setIsGrade(false)
       })
     }
     tae()
-  }, []);
+  }, [])
 
     const sem = data
 
-    const average = "1.00";
+    const average = "1.00"
 
     //display grade
     const displayGrade = async (e) => {
       let selected = JSON.parse(e.target.value)
-      console.log(selected);
-
+      console.log(selected)
+      setSelected(true)
+      setLoading(true)
       await api.post('userGrades', selected).then(
         response => {
           setGrades(response.data)
           console.log(response.data)
           renderTable()
+          setLoading(false)
         }
       ).catch((err) => {
         console.log(err)
+        setLoading(false)
       })
     }
 
@@ -78,26 +86,43 @@ function EnrolledSubjects() {
                   </select>
                   </div>
             </div>
-            <div className="card mt-3 table-holder">
-              <table className="table my-0 table-striped table-hover" id="dataTable">
-                  <thead>
-                    <tr>
-                      <th>Subject Code</th>
-                      <th>Description</th>
-                      <th>Grade</th>
-                      <th>Completion</th>
-                      <th>Unit</th>
-                      <th>Credit Unit</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {renderTable()}
-                  </tbody>
-                </table>
-                <div className="mt-2 mr-2 mb-2 px-3 text-right">
-                    Average: {average}
-                </div>
-            </div>
+            {
+              isSelected ? 
+              <>
+              <div className="card mt-3 table-holder">
+                <table className="table my-0 table-striped table-hover" id="dataTable">
+                {
+                    (loading) ?
+                    <div className="d-flex justify-content-center">
+                        <Spinner className="d-flex justify-content-center" animation="border" role="status"/>
+                    </div>
+                    :
+                    <>
+                    <thead>
+                      <tr>
+                        <th>Subject Code</th>
+                        <th>Description</th>
+                        <th>Grade</th>
+                        <th>Completion</th>
+                        <th>Unit</th>
+                        <th>Credit Unit</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {renderTable()}
+                    </tbody>
+                    <tfoot className="mt-2 mr-2 mb-2 px-3 text-right">
+                      Average: {average}
+                    </tfoot>
+                    </>
+                }
+                  </table>
+              </div>
+              </> :
+              <div className="card mt-3 table-holder text-center">
+                Select the semester and year
+              </div>
+            }
           </>
         }
     </div>
