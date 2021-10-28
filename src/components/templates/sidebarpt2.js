1ed9c8bcfe47e6412e47259dafce281d65851bbf
lcsaria <lmarlsaria21@gmail.com>
2021-10-28 11:31:06 +0800

    update sidebar

--- src/components/templates/sidebarpt2.js
+++ src/components/templates/sidebarpt2.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../../assets/css/latestsidebar.css'
 import {Sidebardata} from './sidebardata'
 import logo from '../../assets/logo.png'
+import { Collapse, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { left } from '@popperjs/core';
 
 
@@ -10,18 +11,27 @@ const Sidebar = () => {
 
     const handlemeow = () => {
         setShowNav(!showNav)
+        console.log(showNav)
     }
 
     return (
         <div className={showNav ? "sidebars d-none d-lg-block" : "sidebars collapsed d-none d-lg-block"}>
             <ul className="sidebarlist">
                 <div className="meow row">
-                    <div className="col-10">
-                        <img className="sidebar-logo ml-2" src={logo} alt="logo"/>  
-                    </div>
-                    <div className="col-2">
-                        <i className="fas fa-bars sidebar-icon" onClick={handlemeow}/>
-                    </div>
+                    {
+                        showNav ? 
+                        <>
+                        <div className="col-10">
+                            <img className="sidebar-logo ml-2" src={logo} alt="logo"/>  
+                        </div>
+                        <div className="col-2">
+                            <i className="fas fa-bars sidebar-icon" onClick={handlemeow}/>
+                        </div>
+                        </> :
+                        <>
+                            <i className="fas fa-bars text-center" onClick={handlemeow}/>
+                        </>
+                    }
                 </div>
                 {Sidebardata.map((val, key) => {
                     return(
@@ -30,8 +40,24 @@ const Sidebar = () => {
                         onClick={() => {window.location.pathname = val.link}}
                         id={window.location.pathname === val.link ? "active" : ""}
                         > 
-                        <div id="icon">{val.icon}</div>
-                        <div id="title">{val.title}</div>
+                        {
+                            showNav ?
+                            <>
+                            <div id="icon">{val.icon}</div>
+                            <div id="title">{val.title}</div>
+                            </> : 
+                            <>
+                            <OverlayTrigger 
+                                placement="right"
+                                overlay={
+                                    <Tooltip>{val.title}</Tooltip>
+                                }
+                            >
+                            <div id="icon">{val.icon}</div>
+                            </OverlayTrigger>
+                            </>
+                        }
+                        
                         </li>
                     )
                 })}
