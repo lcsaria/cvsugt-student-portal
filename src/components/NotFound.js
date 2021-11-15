b0c43174602d3027e18db408e2e7ed3479a74f52
bjpogs <bjpogs26@gmail.com>
2021-11-15 15:29:13 +0800

    updates

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -13,23 +13,8 @@ function NotFound() {
                          <div className="d-sm-flex justify-content-between align-items-center mb-4" />
                      </div>
                     <div className="container">
-                        { /* 
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                <span className="card-title card text-center p-2"
-                                style={{position: "relative", top: "-30px",  border: 
-                                "1px black solid",display: 'block',
-                                width: '200px'}}>
-                                <b className="text-uppercase text-black">Account Settings</b>
-                                </span>
-
-                                //CONTENT HERE MTHRFCKR  
-                                
-                            </div>
-                        </div>
-                        */ }
-
-                        <h1><i class="fas fa-hammer mr-3"/>PAGE NOT FOUND.</h1>
+                        
+                        <h1><i class="fas fa-exclamation-triangle mr-3"/>PAGE NOT FOUND.</h1>
                         <Button variant="success" onClick={()=> window.location.href = '/dashboard'}>
                             Back to Homepage
                         </Button>
